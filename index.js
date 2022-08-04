e62b4b7046fcba9140f97938d24cd7ec06f7ec1b
iamkibeh <kibet.immanuel@student.moringaschool.com>
2022-08-04 10:09:17 +0300

    Complete the assignment

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Code your solutions in this file
+// const gifts = ["teddy bear", "drone", "doll"];
+
+// function wrapGifts(gifts) {
+//   for (let i = 0; i < gifts.length; i++) {
+//     console.log(`Wrapped ${gifts[i]} and added a bow!`);
+//     debugger;
+//   }
+
+//   return gifts;
+// }
+
+// wrapGifts(gifts);
+
+function writeCards(arr, event) {
+    let newArr = [];
+for(let i=0; i<arr.length; i++) {
+    let message =`Thank you, ${arr[i]}, for the wonderful ${event} gift!`
+    newArr.push(message);
+}
+return newArr
+}
+
+// console.log(writeCards(["Charlie", "Samip", "Ali"],"birthday"))
+
+function countDown(num) {
+while (num>=0) {
+    console.log(num)
+    num--;
+}
+}
+
+console.log( countDown(10))
